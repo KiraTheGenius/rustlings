087d45ce18ec2e34c18e34d8c562d1571cc58b72
Amir Valipour <amir.v@mywehub.io>
2024-02-21 21:49:08 +0330

    Solve a lot:/

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM DONE
+
+fn main() {
+    fn call_me() {};
+    call_me();
+}
---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,14 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM DONE
+
+fn main() {
+    let name = "Amir";
+    let x = 5;
+    println!("Hello {}!, {}", name, x);
+}
